9f931c92922751e3e9182edaaf5901734f90d439
Ahrar Hussain <ahrarhakeem@gmail.com>
2022-02-08 12:18:31 +0530

    Frontend Files

---
+++ whatsapp-frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0MtFlN-hHsAfUPbu_ProQor5dO2Pdjow",
+  authDomain: "whatsapp-mern-113.firebaseapp.com",
+  projectId: "whatsapp-mern-113",
+  storageBucket: "whatsapp-mern-113.appspot.com",
+  messagingSenderId: "218601664108",
+  appId: "1:218601664108:web:309aa4edbadc5e27f23249"
+};
